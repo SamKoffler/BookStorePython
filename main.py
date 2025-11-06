d41a572f9ddf6c5a090951093e99b6f6ec220532
Sam Koffler <smjobob@gmail.com>
2025-11-05 19:30:28 -0500

    Add Book and Inventory classes in main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# class Store:
+#     def __init__(self, title, author, id):
+#         self.title = title
+#         self. author = author
+#         self.id = id
+
+class Book:
+    def __init__(self, title, author, id):
+        self.title = title
+        self. author = author
+        self.id = id
+    
+    
+class Inventory:
+    def __init__(self, books={}):
+        self.inventory = book
+    
+    def add_to_inventory(book):
+        
+        
+      
+theBible = Book("The Holy Bible", "God", 1234)   
+
+print(theBible.id)
