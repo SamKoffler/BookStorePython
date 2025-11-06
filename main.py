c7da28f908a06859b138632e332b4350ed6f247b
Sam Koffler <smjobob@gmail.com>
2025-11-05 19:42:06 -0500

    Refactor Inventory class and update book IDs

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 #         self. author = author
 #         self.id = id
 
+
 class Book:
     def __init__(self, title, author, id):
         self.title = title
@@ -12,13 +13,23 @@ class Book:
     
     
 class Inventory:
-    def __init__(self, books={}):
-        self.inventory = book
+    def __init__(self):
+        self.inventory = {}
     
-    def add_to_inventory(book):
-        
+    def add_to_inventory(self, book):
+        if book.title not in self.inventory:
+            self.inventory[book.title] = 1
+        else:
+            self.inventory[book.title] += 1
         
       
-theBible = Book("The Holy Bible", "God", 1234)   
+theBible = Book("The Holy Bible", "God", "0001")
 
-print(theBible.id)
+warAndPeace = Book("War and Peace", "Leo Tolstoy", "0002")
+
+myInventory = Inventory()
+
+myInventory.add_to_inventory(theBible)
+myInventory.add_to_inventory(warAndPeace)
+
+print(myInventory.inventory)
